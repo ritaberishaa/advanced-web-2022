dd62649617b22cb7587e41f91629ceeaf95944b6
ritaberishaa <76747778+ritaberishaa@users.noreply.github.com>
2022-03-07 19:38:07 +0100

    First commit

---
+++ backend/src/main/java/com/cacttus/rita/advanced/web/AdvancedWebApplication.java
@@ -0,0 +1,13 @@
+package com.cacttus.rita.advanced.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedWebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cacttus/rita/advanced/web/AdvancedWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacttus.rita.advanced.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
